731249159b0800eaa793749e9a1b7fb368790874
Neil <neilpatil@live.com>
2019-10-18 15:34:14 +0100

    Deleted ProfileQuery

--- ProfileQuery/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (context, input) {
-    var documents = context.bindings.documents;
-    for (var i = 0; i < documents.length; i++) {
-        var document = documents[i];
-        console.log(document);
-        // operate on each document
-    }
-    context.done();
-};
