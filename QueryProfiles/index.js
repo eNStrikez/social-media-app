cde404378656e98dc85d1c21e5a54e152bad6f85
Neil <neilpatil@live.com>
2019-10-18 16:32:48 +0100

    Added output for QueryProfiles

--- QueryProfiles/index.js
+++ QueryProfiles/index.js
@@ -1,16 +1,13 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+module.exports = function (context, req, profiles) {
+    context.log('Searching for profiles...');
+    if (!profiles)
+    {
+        context.log("No profiles found");
+    }
+    else
+    {
+        context.log(profiles);
+    }
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
+    context.done();
 };
