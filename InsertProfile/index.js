9972abe8611ed2bed724dea4101c00c1255217b3
Neil Patil <neilpatil@live.com>
2019-10-26 20:52:46 +0100

    Changed tag to id in insert

--- InsertProfile/index.js
+++ InsertProfile/index.js
@@ -1,9 +1,9 @@
 module.exports = function (context, req) {
     context.log(req.body.tag);
     context.bindings.profileDocument = JSON.stringify({
+        id: req.body.tag,
         name: req.body.name,
         password: req.body.password,
-        tag: req.body.tag,
         icon: "null",
         followers: [],
         following: []
