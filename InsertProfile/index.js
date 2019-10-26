826fb27a2caad1691f2b69d8cb09d11c1cf47d8d
Neil Patil <neilpatil@live.com>
2019-10-26 12:08:01 +0100

    Changed to use single document insertion

--- InsertProfile/index.js
+++ InsertProfile/index.js
@@ -1,12 +1,16 @@
-module.exports = function (context, req) {
-    context.log('Inserting documents...');
-    context.bindings.documentsToStore = [];
-    for(let i = 0, len=req.body.length; i<len;i++){
-        const doc = req.body[i];
-        context.bindings.documentsToStore.push(doc);
-    }
+module.exports = function (context) {
+
+    context.bindings.employeeDocument = JSON.stringify({
+        name: context.bindings.myQueueItem.name,
+        password: context.bindings.myQueueItem.password,
+        tag: context.bindings.myQueueItem.tag,
+        icon: "null",
+        followers: [],
+        following: []
+    });
 
     context.done(null, {
-        status: 201, body: 'Processed ' + req.body.length + ' documents'
+        status: 201,
+        body: "Successfully inserted profile"
     });
 };
