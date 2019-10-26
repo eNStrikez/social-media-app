035b47e90cbed660f40dd58945ade9ccc7be7eeb
Neil Patil <neilpatil@live.com>
2019-10-26 21:33:11 +0100

    Removed id lookup

--- UpdateProfile/index.js
+++ UpdateProfile/index.js
@@ -1,28 +1,17 @@
 module.exports = function (context, req, profileToUpdate) {
-    context.log('Searching for profiles...');
-    if (!profileToUpdate)
-    {
-        context.res = {
-            status:404,
-            body: "No matching profile found, cannot update"
-        };
-    }
-    else
-    {
-        context.log('Editing found profile');
-        profileToUpdate = JSON.stringify({
-            id: req.body.tag,
-            name: req.body.name,
-            password: req.body.password,
-            icon: req.body.icon,
-            followers: req.body.followers,
-            following: req.body.following
-        });
-        context.res = {
-            status: 200,
-            body: "Updated profile"
-        };
-    }
+    context.log('Editing found profile');
+    profileToUpdate = JSON.stringify({
+        id: req.body.tag,
+        name: req.body.name,
+        password: req.body.password,
+        icon: req.body.icon,
+        followers: req.body.followers,
+        following: req.body.following
+    });
+    context.res = {
+        status: 200,
+        body: "Updated profile"
+    };
     context.log('Returning context');
     context.done();
 };
