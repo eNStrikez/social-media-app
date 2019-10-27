0d37403c0cfaca5c5e58793a3800c3fcd533a13d
Neil Patil <neilpatil@live.com>
2019-10-27 11:39:52 +0000

    Added function to query posts Added function to add posts (empty) Parametricised QueryProfile

---
+++ QueryPost/index.js
@@ -0,0 +1,20 @@
+module.exports = function (context, req, posts) {
+    context.log('Searching for profiles...');
+    if (!posts)
+    {
+        context.res = {
+            status:404,
+            body: "No posts found, try following some users"
+        };
+    }
+    else
+    {
+        context.log('Responding with found posts...');
+        context.res = {
+            status: 200,
+            body: posts
+        };
+    }
+    context.log('Returning context');
+    context.done();
+};
