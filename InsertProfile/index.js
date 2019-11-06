58291aac926bc9c4544857c89f58df1d1f3932d0
Neil <neilpatil@live.com>
2019-11-06 23:54:30 +0000

    Added validation for existing profiles on insertion

--- InsertProfile/index.js
+++ InsertProfile/index.js
@@ -1,20 +1,27 @@
 module.exports = function (context, req) {
-    context.log(req.body.tag);
-
-    context.bindings.profileDocument = JSON.stringify({
-        id: req.body.tag,
-        name: req.body.name,
-        password: req.body.password,
-        icon: req.body.icon,
-        followers: [],
-        following: [],
-        email: req.body.email
-    });
-
-    context.res = {
-        status: 201,
-        body: "Successfully inserted post"
-    };
-
+    if (!profiles || profiles == [])
+    {
+        context.bindings.profileDocument = JSON.stringify({
+            id: req.body.tag,
+            name: req.body.name,
+            password: req.body.password,
+            icon: req.body.icon,
+            followers: [],
+            following: [],
+            email: req.body.email
+        });
+    
+        context.res = {
+            status: 201,
+            body: "Successfully added profile"
+        };
+    }
+    else
+    {
+        context.res = {
+            status: 404,
+            body: "Profile with that username already exists"
+        };
+    }
     context.done();
 };
