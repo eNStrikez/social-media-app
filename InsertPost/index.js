b3f00ae24ed6bc464b82ff838620b09d2fb40caf
Neil <neilpatil@live.com>
2019-11-06 22:56:06 +0000

    Updated post insertion with correct parameters

--- InsertPost/index.js
+++ InsertPost/index.js
@@ -11,10 +11,13 @@ module.exports = function (context, req) {
 
     context.bindings.postDocument = JSON.stringify({
         tag: req.body.tag,
+        name: req.body.name,
+        icon: req.body.icon,
         timestamp: hours + ":" + mins + ":" + seconds,
         date: day + "/" + month + "/" + year,
         content: req.body.content,
-        media: req.body.media
+        media: req.body.media,
+        tags: req.body.tags
     });
 
     context.done(null, {
