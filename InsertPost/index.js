f7b20a52a411f714640e454fcad9e2921dd43fc8
Neil <neilpatil@live.com>
2019-11-10 21:05:38 +0000

    Changed timestamp generation

--- InsertPost/index.js
+++ InsertPost/index.js
@@ -1,16 +1,5 @@
 module.exports = function (context, req) {
     let date = new Date();
-
-    let seconds = date.getSeconds();
-    if(seconds.length == 1)
-        seconds = '0' + seconds;
-    let mins = date.getMinutes();
-    if(mins.length == 1)
-        mins = '0' + mins;
-    let hours = date.getHours();
-    if(hours.length == 1)
-        hours = '0' + hours;
-
     let day = date.getDate();
     let month = date.getMonth() + 1;
     let year = date.getFullYear()
@@ -19,7 +8,7 @@ module.exports = function (context, req) {
         tag: req.body.tag,
         name: req.body.name,
         icon: req.body.icon,
-        timestamp: hours + ":" + mins + ":" + seconds,
+        timestamp: date.toLocaleDateString(),
         date: day + "/" + month + "/" + year,
         content: req.body.content,
         media: req.body.media,
